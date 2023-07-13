11bdfb0821b9ea08583467d94a06d34facd6151f
praveenkumarbeera <praveen143191@gmail.com>
2023-07-13 13:11:16 +0530

    first commit

---
+++ letsUpgrade/src/main/java/letsUpgrade/Day2Assignment.java
@@ -0,0 +1,19 @@
+package letsUpgrade;
+import java.util.Scanner;
+
+public class Day2Assignment {
+    public static void maindd(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the numbers to add");
+
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=sc.nextInt();
+        int e=sc.nextInt();
+
+        int f=a+b+c+d+e;
+        System.out.println("sum of the given Integers = " + f);
+
+sc.close(); }
+}
